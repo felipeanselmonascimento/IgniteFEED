15ac8e991ad0a554b39ba7975ede565b315ddfb5
felipe239989 <felipeanselmodonascimento@gmail.com>
2022-12-10 20:30:53 -0300

    finishing sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,20 @@
 import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react' //importando os icones
 
 export function Sidebar () {
     return (
         <aside className={styles.sidebar}>
             <img className={styles.cover} src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60v"/>
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/felipeanselmonascimento.png" />
                 <strong>Felipe Anselmo</strong>
                 <span>Web Developer</span>
             </div>
             <footer>
-                <a href='#'>Edit Profile!</a>
+                <a href='#'>
+                    <PencilLine size={24}/>
+                    Edit Profile!
+                </a>
             </footer>
         </aside>
     )
